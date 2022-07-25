55a4383dc55390c3f865b98a37736162b64f0115
Jackie <jye0325@gmail.com>
2022-07-25 03:56:15 -0400

    commit tree e10da8b4b11942700ca1f6329a83da2cccc2685b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+  # attr_accessor :name [Shorthand for getter and setter]
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  # attr_accessor :breed
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 
 end
